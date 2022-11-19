5c34217831948506726c1692c7130880b5af675a
zzzzzzasd123 <112676929+zzzzzzasd123@users.noreply.github.com>
2022-11-19 15:56:28 +0900

    Add files via upload

---
+++ BP_05_EX_Ch7.ipynb.py
@@ -0,0 +1,22 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:/Users/Administrator/Desktop/41.py ================
+이름 : 손주진
+=====================
+Happy Birthday to you!
+Happy Birthday to you!
+Happy Birthday, dear 손주진
+Happy Birthday to you!
+첫 번째 정수: 10
+두 번째 정수: 20
+정수 10+20의 합은? 
+반지름이 5인 원의 면적 :  78.5
+반지름이 5인 원의 둘레 :  31.400000000000002
+첫 번째 정수: 20
+두 번째 정수: 10
+==================
+(20 + 10) = 30
+(20 - 10) = 10
+(20 * 10) = 200
+(20 / 10) = 2
